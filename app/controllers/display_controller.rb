c9d16482f4c2a88d0fc029685f1f8f324d049f30
Michael Grein <michaelgrein92@gmail.com>
2017-02-06 09:47:41 -0500

    Set up admin page to customize content

--- app/controllers/display_controller.rb
+++ app/controllers/display_controller.rb
@@ -1,17 +1,19 @@
 class DisplayController < ApplicationController
+  layout "application", :except => [:index]
+
   def index
     @display = Display.first
 
     random = rand(1000)
 
-    current_weather = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?q=saintpetersburg,us&units=imperial&APPID=44e5ea9665216f4fa7621cf0ce5b3732&#{random}")
+    current_weather = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?q=clearwater,us&units=imperial&APPID=44e5ea9665216f4fa7621cf0ce5b3732&#{random}")
 
     @current_condtion   = current_weather["weather"].map {|result| result["main"]}.first
     @current_temp       = current_weather["main"]["temp"].to_s.split(".").first.to_i
     @weather_icon_code  = current_weather["weather"].map {|result| result["id"]}.first
     @current_location   = current_weather["name"]
 
-    forecast = response = HTTParty.get("http://api.openweathermap.org/data/2.5/forecast/daily?q=saintpetersburg,us&units=imperial&APPID=44e5ea9665216f4fa7621cf0ce5b3732&#{random}")
+    forecast = response = HTTParty.get("http://api.openweathermap.org/data/2.5/forecast/daily?q=clearwater,us&units=imperial&APPID=44e5ea9665216f4fa7621cf0ce5b3732&#{random}")
     @forecast_city      = forecast["city"]["name"]
 
     @next_day_high      = forecast["list"][0]["temp"]["max"].to_s.split(".").first.to_i
@@ -25,5 +27,8 @@ class DisplayController < ApplicationController
     @third_day_high     = forecast["list"][2]["temp"]["max"].to_s.split(".").first.to_i
     @third_day_low      = forecast["list"][2]["temp"]["min"].to_s.split(".").first.to_i
     @third_day_icon     = forecast["list"][2]["weather"].first["id"]
+  end
+
+  def admin
   end
 end
